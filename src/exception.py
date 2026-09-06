ca6745865ab3e546ec19e024fa3dfca8b395c60f
devangtiwari <dewangtiwari69@gmail.com>
2026-09-06 11:51:17 +0530

    EDA and problem statements

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@ import sys
 import logging
 
 
+
 def error_message_detail(error, error_detail: sys):
 
     _, _, exc_tb = error_detail.exc_info()
@@ -30,5 +31,12 @@ class CustomerException(Exception):
 
     def __str__(self):
         return self.error_msg
+
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e :
+        logging.info("Divide by Zero")
+        raise CustomerException(e,sys)
 
 
